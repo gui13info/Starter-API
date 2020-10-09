16b58111363599c955ec463f8ddef46226ec586f
Guilherme Marques <marques.guilherme0067@gmail.com>
2020-10-09 17:57:04 -0300

    Conectando database

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
+//Iniciando o APP
 const app = express();
+
+//Iniciando o DB
+mongoose.connect('mongodb://localhost:27017/nodeapi', 
+    { useNewUrlParser: true }
+);
+
 
 app.get('/', (req, res) => {
     res.send('Hello Rocketseat');
